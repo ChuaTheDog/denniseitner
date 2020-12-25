9798a591079c90a66a9000004ce38f7f9d213abe
Dennis Eitner <denniseitner@gmail.com>
2020-12-25 20:41:22 +0100

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,14 +37,6 @@ module.exports = {
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
-				name: 'images',
-				path: './src/images/',
-			},
-			__key: 'images',
-		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
 				name: `src`,
 				path: `${__dirname}/posts`,
 				name: `posts`,

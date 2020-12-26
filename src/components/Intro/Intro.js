9b111a7c20d197a60592032395a6cf7b84eac7bc
Dennis Eitner <denniseitner@gmail.com>
2020-12-26 10:43:05 +0100

    intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,7 +2,22 @@ import React from 'react';
 import IntroStyle from './intro.module.scss';
 
 const Intro = () => {
-	return <div className={IntroStyle.intro}>Intro</div>;
+	return (
+		<div className={IntroStyle.intro}>
+			<div className='container'>
+				<h1>
+					My Name is <span className='yellow'>Dennis</span> and I create digital
+					experiances.
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+					mollitia aperiam beatae iste similique impedit, fugit suscipit iure,
+					laboriosam iusto magnam eius excepturi dignissimos omnis quidem, eaque
+					itaque consequatur qui!
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Intro;

8c41f8ce579f45e1b6c39830932d8e6e97407262
Dennis Eitner <denniseitner@gmail.com>
2021-01-23 12:31:11 +0100

    updated styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,10 +5,30 @@ import Content from '../components/Content/Content';
 const AboutPage = () => (
 	<main>
 		<Header title={'Hello World'}></Header>
-		<Content
-			content={
-				'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
-			}></Content>
+		<div className='container'>
+			<div className='columns mt-6'>
+				<div className='column is-half is-offset-one-quarter'>
+					Mit Wireframes und ausgearbeiteten Prototypen visualisiere ich die
+					User Stories, um diese vor der Umsetzung testen zu können. In
+					regelmässigen Kickoffs und Walkthroughs vor den Sprints engagiere mich
+					in agilen Arbeitsprozessen, wodurch ein teamübergreifendes Verständnis
+					der Entwicklungsziele entsteht. Daraus resultiert eine progressive
+					Produktentwicklung. Zu meinen täglichen Aufgaben gehören unter anderem
+					die Entwicklung von Datenmodellen, UML, Flowmaps, Sitemaps, Mockups,
+					Designvorlagen, Informationsarchitektur, Interaction- und
+					Userinterface-Design, Testing, User Stories. Ausserdem tausche ich
+					mich regelmässig mit Business Stakeholders aus und verwalte den
+					Backlog. Meine Leidenschaft ist die Realisation von Ideen und
+					Konzepten. Dabei habe ich eine ausgeprägte unternehmerische und
+					kommerzielle Denkweise. Durch meine Aus-und Weiterbildung decke ich
+					ein breites Spektrum von Disziplinen in der Entwicklung von digitalen
+					Produkten ab. So bin ich in der Lage komplexe technische Sachverhalte
+					rasch zu verstehen. Ich lege grossen Wert darauf Form und Funktion
+					optimal miteinander zu verbinden und dadurch ein aussergewöhnliches
+					und nachhaltiges Erlebnis für die Endnutzer zu schaffen.
+				</div>
+			</div>
+		</div>
 	</main>
 );
 

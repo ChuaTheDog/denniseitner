1ae9c4531ee94963047e5b4468790cbd5b7fe76f
Dennis Eitner <denniseitner@gmail.com>
2021-04-26 16:48:54 +0200

    seo

--- gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from './src/components/Layout/Layout';
-
-require('typeface-pacifico');
-
-export function wrapPageElement({ element, props }) {
-	return <Layout {...props}>{element}</Layout>;
-}

0fef381e28fb0fc717248d715d20a13b471066d0
Dennis Eitner <denniseitner@gmail.com>
2021-09-10 10:44:14 +0200

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,10 +20,12 @@ const Header = (props) => {
 						<div className={headerStyles.introInfo}>
 							<h1>Hi, I'm Dennis. </h1>
 							<p>
-								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod
-								quibusdam nisi ex eveniet corrupti, expedita repudiandae debitis
-								libero numquam officiis velit voluptates esse iure autem eius
-								cum voluptatibus enim dolores!
+								I am a requirements engineer, product owner/manager, UX designer
+								and frontend developer. Preferably but not necessarily in that
+								order. For over twenty years I have been developing products and
+								services with the greatest passion. No effort too big, no idea
+								too daring. You can{' '}
+								<Link to='/about'> learn more about me here</Link> if you like.
 							</p>
 						</div>
 						<Row>

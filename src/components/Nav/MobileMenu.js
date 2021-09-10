6eb30ef7637438efc0280a00ca9cdf9da45fee0b
Dennis Eitner <denniseitner@gmail.com>
2021-09-10 13:18:47 +0200

    removed blog

--- src/components/Nav/MobileMenu.js
+++ src/components/Nav/MobileMenu.js
@@ -32,14 +32,12 @@ const StyledMenu = styled.nav`
 `;
 
 const MobileMenu = ({ active, open, ...props }) => {
-	const isActive = active ? 'open' : 'closed';
 	const isHidden = open ? true : false;
-	const tabIndex = isHidden ? 0 : -1;
 	return (
 		<StyledMenu open={open} aria-hidden={!isHidden} {...props}>
 			<a href='/about'>About</a>
 			<a href='/friends'>Friends</a>
-			<a href='/blog'>Blog</a>
+
 			<a href='/contact'>Contact</a>
 		</StyledMenu>
 	);

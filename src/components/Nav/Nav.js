e7719fb4b18e2342ebf1645f01b00676d2db750e
Dennis Eitner <denniseitner@gmail.com>
2021-09-10 13:09:31 +0200

    put back nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import * as navStyles from './nav.module.scss';
 import Home from '../../assets/svg/Home.svg';
 import MobileNav from './MobileNav';
 
 const Nav = () => {
+	const [pos, setPos] = useState('top');
+	useEffect(() => {
+		document.addEventListener('scroll', (e) => {
+			let scrolled = document.scrollingElement.scrollTop;
+			if (scrolled >= 400) {
+				setPos('moved');
+			} else {
+				setPos('top');
+			}
+		});
+	}, []);
 	return (
 		<div className={navStyles.navWrapper} id='nav'>
 			<a href='/'>
 				<Home className={navStyles.home}></Home>
 			</a>
 
-			<nav className={navStyles.nav}>
+			<nav
+				className={navStyles.nav}
+				/*style={{
+					backgroundImage:
+						pos === 'top'
+							? ''
+							: 'linear-gradient(#bf1a90 0%, rgba(255, 255, 255, 0) 100%)',
+				}}*/
+				style={{
+					backgroundColor:
+						pos === 'top' ? 'rgba(0, 0, 0, 0)' : 'rgba(0, 0, 0, .5)',
+				}}>
 				<a className={navStyles.navItem} href='/about'>
 					About
 				</a>
@@ -24,6 +47,7 @@ const Nav = () => {
 					Contact
 				</a>
 			</nav>
+			<MobileNav></MobileNav>
 		</div>
 	);
 };

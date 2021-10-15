b9e6c3c5a28ecc8be0a871e5633c2d185da65541
Dennis Eitner <denniseitner@gmail.com>
2021-10-15 12:23:55 +0200

    updated cookie consnet

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,13 +6,21 @@ import Header from '../Header/Header';
 import Nav from '../Nav/Nav';
 import MobileNav from '../Nav/MobileNav';
 import ScrollButton from '../ScrollButton/ScrollButton';
-import CookieConsent from '../CookieBanner/CookieConsent';
+//import CookieConsent from '../CookieBanner/CookieConsent';
+import CookieConsent from 'react-cookie-consent';
+
 import '../../assets/styles/styles.scss';
 
 export default function Layout({ children, withHeader = true }) {
 	return (
 		<>
-			<CookieConsent />
+			<CookieConsent
+				location='bottom'
+				buttonText='Accept'
+				declineButtonText='Decline'
+				cookieName='gatsby-gdpr-google-analytics'>
+				This site uses cookies ...
+			</CookieConsent>
 
 			<Nav />
 			<MobileNav />

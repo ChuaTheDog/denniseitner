40bed2369b7337072ed39ca66fb62d9b20f1125d
Dennis Eitner <denniseitner@gmail.com>
2021-10-15 12:19:48 +0200

    updated cokkie consent

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,13 +6,13 @@ import Header from '../Header/Header';
 import Nav from '../Nav/Nav';
 import MobileNav from '../Nav/MobileNav';
 import ScrollButton from '../ScrollButton/ScrollButton';
-import CookieBanner from '../CookieBanner/CookieBanner';
+import CookieConsent from '../CookieBanner/CookieConsent';
 import '../../assets/styles/styles.scss';
 
 export default function Layout({ children, withHeader = true }) {
 	return (
 		<>
-			<CookieBanner />
+			<CookieConsent />
 
 			<Nav />
 			<MobileNav />

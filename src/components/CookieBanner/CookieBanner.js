f3960b2124a062c9f0b5cd0ba850fb0dd456850c
Dennis Eitner <denniseitner@gmail.com>
2021-10-15 12:02:40 +0200

    CookieBanner

--- src/components/CookieBanner/CookieBanner.js
+++ src/components/CookieBanner/CookieBanner.js
@@ -1,60 +1,64 @@
-import * as React from 'react';
-import { useLocation } from '@reach/router';
+import React from 'react';
+import { useLocation } from '@reach/router'; // this helps tracking the location
 import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies';
-
-function isBrowser() {
-	return typeof window !== 'undefined';
-}
-
-function getValue(key, defaultValue) {
-	return isBrowser() && window.localStorage.getItem(key)
-		? JSON.parse(window.localStorage.getItem(key))
-		: defaultValue;
-}
-
-function setValue(key, value) {
-	window.localStorage.setItem(key, JSON.stringify(value));
-}
-
-function useStickyState(defaultValue, key) {
-	const [value, setter] = React.useState(() => {
-		return getValue(key, defaultValue);
-	});
-
-	React.useEffect(() => {
-		setValue(key, value);
-	}, [key, value]);
-
-	return [value, setter];
-}
+import CookieConsent from "react-cookie-consent"
 
 const CookieBanner = () => {
-	const location = useLocation();
-	if (isBrowser()) {
-		initializeAndTrack(location);
-	}
-
-	const [bannerHidden, setBannerHidden] = useStickyState(
-		false,
-		'consentCookieHidden'
-	);
-
-	const EnableAnalytics = () => {
-		document.cookie = 'gatsby-gdpr-google-analytics=true';
-		setBannerHidden(true);
-	};
-
+	function isBrowser() {
+		return typeof window !== 'undefined';
+	  }
+	  
+	  function getValue(key, defaultValue) {
+		return isBrowser() && window.localStorage.getItem(key)
+		  ? JSON.parse(window.localStorage.getItem(key))
+		  : defaultValue;
+	  }
+	  
+	  function setValue(key, value) {
+		window.localStorage.setItem(key, JSON.stringify(value));
+	  }
+	  
+	  function useStickyState(defaultValue, key) {
+		const [value, setter] = React.useState(() => {
+		  return getValue(key, defaultValue);
+		});
+	  
+		React.useEffect(() => {
+		  setValue(key, value);
+		}, [key, value]);
+	  
+		return [value, setter];
+	  }
+	  
+	  const CookieConsent = () => {
+		const location = useLocation();
+		if (isBrowser()) {
+		  initializeAndTrack(location);
+		}
+	  
+		const [bannerHidden, setBannerHidden] = useStickyState(
+		  false,
+		  'consentCookieHidden',
+		);
+	  
+		const EnableAnalytics = () => {
+		  document.cookie = 'gatsby-gdpr-google-analytics=true';
+		  setBannerHidden(true);
+		};
 	return (
-		<>
-			{!bannerHidden && (
-				<div>
-					<span>
-						We use cookies to personalize content and analyze our traffic.
-					</span>
-					<button onClick={EnableAnalytics}>OK</button>
-				</div>
-			)}
-		</>
+		<div>
+			<CookieConsent
+				cookieName='gatsby-plugin-gdpr-cookies'
+				buttonId='confirm'
+				buttonText='Accept'
+				declineButtonId='decline'
+				declineButtonText='Decline'
+				enableDeclineButton={true}
+				disableStyles={true}
+				onAccept={EnableAnalytics}>
+				<p className='cookieMessage'>This website uses cookies 🍪 </p>
+			</CookieConsent>
+		</div>
 	);
 };
 

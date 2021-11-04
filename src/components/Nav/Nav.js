406699373cef6424b04316556689adfcedec0072
Dennis Eitner <denniseitner@gmail.com>
2021-11-04 14:23:19 +0100

    typogrphy improvements

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -35,22 +35,25 @@ const Nav = () => {
 	const [links] = useState(LINKS);
 
 	return (
-		<div className='navigation__wrapper' id='nav'>
-			<Link to='/' className='navigation__home--link'>
-				<Home className='navigation__home' />
+		<div>
+			<Link to='/' className='homeLink'>
+				<Home className='home' />
 			</Link>
-			<nav
-				className='navigation'
-				style={{
-					backgroundColor:
-						pos === 'top' ? 'rgba(0, 0, 0, 0)' : 'rgba(0, 0, 0, .5)',
-				}}>
-				{links.map((link) => (
-					<Link key={link.title} className='navigation__link' to={link.to}>
-						{link.title}
-					</Link>
-				))}
-			</nav>
+
+			<div className='navigation__wrapper' id='nav'>
+				<nav
+					className='navigation'
+					style={{
+						backgroundColor:
+							pos === 'top' ? 'rgba(0, 0, 0, 0)' : 'rgba(0, 0, 0, .5)',
+					}}>
+					{links.map((link) => (
+						<Link key={link.title} className='navigation__link' to={link.to}>
+							{link.title}
+						</Link>
+					))}
+				</nav>
+			</div>
 		</div>
 	);
 };
